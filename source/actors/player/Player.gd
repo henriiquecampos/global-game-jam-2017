f5ac1f4c91147cd5939162db85dc0e9e0c4a252e
Henrique Campos <henriiquecampos@gmail.com>
2017-01-21 14:28:15 -0200

    implemented player animation

--- source/actors/player/Player.gd
+++ source/actors/player/Player.gd
@@ -6,19 +6,32 @@ export (String, "player_one", "player_two") var player_flag
 func _ready():
 	add_to_group(player_flag)
 	set_fixed_process(true)
-	
 func _fixed_process(delta):
 	#Movement
 	move(Vector2(0,0))
 	if player_flag == "player_one":
 		if Input.is_action_pressed("player_one_up"):
 			move_and_slide(Vector2(0,-SPEED * delta * multiplier))
+			get_node("Sprite").set_animation("running")
+		elif Input.is_action_just_released("player_one_up"):
+			get_node("Sprite").set_animation("iddle")
 		if Input.is_action_pressed("player_one_right"):
+			get_node("Sprite").set_flip_h(false)
 			move_and_slide(Vector2(SPEED * delta * multiplier,0))
+			get_node("Sprite").set_animation("running")
+		elif Input.is_action_just_released("player_one_right"):
+			get_node("Sprite").set_animation("iddle")
 		if Input.is_action_pressed("player_one_left"):
+			get_node("Sprite").set_flip_h(true)
 			move_and_slide(Vector2(-SPEED * delta * multiplier,0))
+			get_node("Sprite").set_animation("running")
+		elif Input.is_action_just_released("player_one_left"):
+			get_node("Sprite").set_animation("iddle")
 		if Input.is_action_pressed("player_one_down"):
+			get_node("Sprite").set_animation("running")
 			move_and_slide(Vector2(0,SPEED * delta * multiplier))
+		elif Input.is_action_just_released("player_one_down"):
+			get_node("Sprite").set_animation("iddle")
 		if Input.is_action_just_pressed("player_one_interact"):
 			interact()
 	elif player_flag == "player_two":
